16d5b0019d1ab1d5c296021b3dac2170bd36032d
bethanymillar <bethany.millar@canterbury.ac.nz>
2025-11-07 19:26:26 +1300

    Update and rename run_lava_flowF_East.sh to run_lava_flowF.sh

--- run_lava_flowF.sh
+++ run_lava_flowF.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Path to the vents file and output directory
-VENTS_FILE="./inputs/vent_source/vents_East_F.utm"  # Assuming you have a specific vents file for Auckland
-OUTPUT_DIR="./flow_outputs/F/flowF_East"
+VENTS_FILE="./inputs/vent_source/vents_F.utm"  
+OUTPUT_DIR="./flow_outputs/F"
 CSV_CLEANER_SCRIPT="./scripts/convert_to_csv_F.py"
 RASTER_CONVERTER_SCRIPT="./scripts/lava_flow_rasterised_F.py"
 
